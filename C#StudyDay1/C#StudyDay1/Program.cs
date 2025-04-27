1010acf34eecee447437d662b3c7696557c64f04
efegsert1 <gjdpma1365@gmail.com>
2025-04-27 10:54:56 +0900

    C#스터디 - 1

---
+++ C#StudyDay1/C#StudyDay1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "한진우";
+int age = 17;
+string job = "학생";
+bool result = true;
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(job);
+Console.WriteLine(result);
